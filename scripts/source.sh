b3e9705e699db26cb63b716b80cb86dafeb14e56
Hernan Garcia <hernangarqui@gmail.com>
2025-11-23 22:54:46 -0600

    FIX IN SOURCE

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,10 +2,18 @@
 
 # Set environment variables for Hadoop user
 
-cat >> /home/hadoop/.bashrc << 'EOF'
+# First, check if already configured to avoid duplicates
+if ! grep -q "HADOOP_HOME" /home/hadoop/.bashrc; then
+    cat >> /home/hadoop/.bashrc << 'EOF'
+
+# System PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+# Java
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')
+export PATH=$PATH:$JAVA_HOME/bin
 
 # Hadoop Environment Variables
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export HADOOP_HOME=/usr/local/hadoop
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export HADOOP_MAPRED_HOME=$HADOOP_HOME
@@ -13,10 +21,12 @@ export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
 export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
-export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')
-export PATH=$PATH:$JAVA_HOME/bin
 
 EOF
+fi
 
-# Source it immediately for current session
-su - hadoop -c "source /home/hadoop/.bashrc"
+# Also set JAVA_HOME in hadoop-env.sh
+JAVA_PATH=$(readlink -f /usr/bin/java | sed 's:/bin/java::')
+sed -i "s|# export JAVA_HOME=|export JAVA_HOME=$JAVA_PATH|g" /usr/local/hadoop/etc/hadoop/hadoop-env.sh
+
+chown hadoop:hadoop /home/hadoop/.bashrc
