be4fa78ec78c3cb2594ccac0976a542e464197e4
Hernan Garcia <hernangarqui@gmail.com>
2025-11-23 23:45:53 -0600

    fix: correct aux-services property name in yarn-site.xml and update Hadoop environment path in update_java_home.sh

--- scripts/update_java_home.sh
+++ scripts/update_java_home.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-HADOOP_ENV="/usr/local/hadoop-3.3.6/etc/hadoop/hadoop-env.sh"
+# chANGED: Path fixed from hadoop-3.3.6 to hadoop
+HADOOP_ENV="/usr/local/hadoop/etc/hadoop/hadoop-env.sh"
 JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
 
 sed -i "s|^export JAVA_HOME=.*|export JAVA_HOME=$JAVA_HOME|" "$HADOOP_ENV"
